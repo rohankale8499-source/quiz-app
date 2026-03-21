43ab07ba91fe42f57eaee6f0cc0fec36b7c9bdc4
Rohan Kale <rohankale8499@gmail.com>
2026-03-21 18:20:41 +0530

    Done

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-ns9hes03p%34w*%vnoh%#l=^h+h6$_n0sa_ox5m&lpwb=du!71'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
